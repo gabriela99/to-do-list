c096caa70179496e7abb910d3232e4072018851b
[Gabriela] <gabriela.sikora@code.berlin>
2020-01-04 16:54:14 +0100

    Add gitignore

---
+++ src/components/Todos.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import TodoItem from './TodoItem';
+import PropTypes from 'prop-types';
+
+class Todos extends React.Component {
+    markComplete = () => {
+        console.log('hello')
+    }
+
+  render() {
+    return this.props.todos.map((todo) => (
+    <TodoItem key={todo.id} todo={todo} markComplete={this.props.markComplete}/>
+    ));
+  }
+}
+
+// PropTypes
+Todos.propTypes = {
+    todos: PropTypes.array.isRequired
+}
+
+export default Todos;
