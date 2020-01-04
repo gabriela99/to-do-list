dc7a916a85bdda1958114496c878741797a60157
[Gabriela] <gabriela.sikora@code.berlin>
2020-01-04 14:38:13 +0100

    Add list styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
 import React from 'react';
 import './App.css';
+import Todos from './components/Todos';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>app</h1>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    todos: [
+      {
+        id:1,
+        title: 'Take out the trash',
+        completed: false
+      },
+      {
+        id:2,
+        title: 'Cook dinner',
+        completed: true
+      },
+      {
+        id:3,
+        title: 'Scrub the shower',
+        completed: false
+      }
+    ]
+  }
+
+
+  render() {
+    return (
+      <div className="App">
+        <Todos todos={this.state.todos}/>
+      </div>
+    );
+  }
 }
 
 export default App;
